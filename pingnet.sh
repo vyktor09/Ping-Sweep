139f3f7773391704e44127e2f187fe93def457b6
Victor Leite <45108137+vyktor09@users.noreply.github.com>
2021-01-18 08:24:06 -0500

    Add files via upload

---
+++ pingnet.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ "$1" == "" ]
+then
+	echo "INFORMATION SECURITY - PING SWEEP"
+	echo "Modo de Uso: $0 REDE" 
+	echo "EXEMPLO: $0 192.168.0"
+else
+
+for host in {1..254};
+do
+ping -c 1 $1.$host | grep "64 bytes" | cut -d ":" -f 1| cut -d " " -f 4;
+done
+fi
